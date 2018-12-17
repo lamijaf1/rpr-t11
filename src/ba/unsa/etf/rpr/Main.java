193eacd83915f09b964cc4c63c3718d11eb54129
Lamija <lfazlija1@etf.unsa.ba>
2018-12-17 23:21:42 +0100

    dodana fja za kreiranje baze

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,11 +1,31 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
+        createNewDatabase("baza");
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
     }
