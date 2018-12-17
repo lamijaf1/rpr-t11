fa1d77f817a7742c47258a9b7f547b1ea3f8549a
Lamija <lfazlija1@etf.unsa.ba>
2018-12-17 18:35:54 +0100

    dodani geteri,seteri i konstruktor i metoda setGlavnigrad u klasi drzava

--- src/ba/unsa/etf/rpr/Drzava.java
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -1,6 +1,38 @@
 package ba.unsa.etf.rpr;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Drzava {
-    public void setGlavniGrad(Grad sarajevo) {
+    private SimpleIntegerProperty id= new SimpleIntegerProperty(0);
+    private SimpleStringProperty naziv = new SimpleStringProperty("");
+    private SimpleIntegerProperty broj_stanovnika = new SimpleIntegerProperty(0);
+    private Grad glavni_grad;
+
+    public Drzava(int id, String naziv, int broj_stanovnika, Grad glavni_grad) {
+        this.setId(new SimpleIntegerProperty(id));
+        this.setNaziv(new SimpleStringProperty(naziv));
+        this.setBroj_stanovnika(new SimpleIntegerProperty(broj_stanovnika));
+        setGlavniGrad(glavni_grad);
     }
+
+
+    public void setGlavniGrad(Grad glavni_grad) {
+        this.setGlavni_grad(glavni_grad)
+    }
+
+    public SimpleIntegerProperty idProperty() {return id; }
+    public int getId() { return id.get();}
+    public void setId(int id) { this.id.set(id); }
+
+    public String getNaziv() { return naziv.get(); }
+    public SimpleStringProperty nazivProperty() { return naziv; }
+    public void setNaziv(String naziv) { this.naziv.set(naziv); }
+
+    public SimpleIntegerProperty broj_stanovnikaProperty() {return broj_stanovnika; }
+    public void setBroj_stanovnika(int broj_stanovnika) {this.broj_stanovnika.set(broj_stanovnika); }
+    public void getBroj_stanovnika(){ return broj_stanovnika.get(); }
+
+    public Grad getGlavni_grad() { return glavni_grad; }
+    public void setGlavni_grad(Grad glavni_grad) { this.glavni_grad = glavni_grad; }
 }
