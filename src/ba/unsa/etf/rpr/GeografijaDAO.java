bfb9c9617f73002249d1e9996896a35a29d82d75
Lamija <lfazlija1@etf.unsa.ba>
2018-12-17 20:25:14 +0100

    azuriranje klase GeografijaDAO

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,18 +1,57 @@
 package ba.unsa.etf.rpr;
 
+import javafx.beans.property.SimpleObjectProperty;
+
+import java.sql.*;
 import java.util.ArrayList;
 
 public class GeografijaDAO {
-    public static void removeInstance() {
+    ArrayList<Grad> gradovi=new ArrayList<>();
+    ArrayList<Drzava> drzave=new ArrayList<>();
+    private static GeografijaDAO instance = null;
+
+    private Connection conn;  /* i ostalo što treba za bazu */
+    String url = "jdbc:sqlite:resources/baza.db";
+    private PreparedStatement stmt1,stmt2;
+
+
+    private GeografijaDAO() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:sqlite:resources/baza.db");
+            stmt1 = conn.prepareStatement("SELECT id, naziv,  broj_stanovnika FROM main.grad WHERE main.drzava.naziv= ?");
+            ResultSet rs = stmt1.executeQuery();
+            while (rs.next()) {
+                // System.out.println()
+                Grad k = new Grad(rs.getInt(1), rs.getString(2), rs.getInt(3));
+                gradovi.add(k);
+            }
+            // ne znam postaviti strani kljuc za ovaj slucaj pa ne mogu odabrati dobro
+            stmt2=conn.prepareStatement("SELECT id, naziv FROM main.drzava WHERE main.grad=?");
+            ResultSet rs = stmt2.executeQuery();
+            while (rs.next()) {
+                // System.out.println()
+                Drzava d=new Drzava(rs.getInt(1), rs.getString(2), rs.getInt(3));
+                drzave.add(d);
+            }
+
+        } catch(SQLException e) {
+            System.out.println("Neuspješno čitanje iz baze: " + e.getMessage());
+        }
+
     }
 
+
     public static GeografijaDAO getInstance() {
-        GeografijaDAO instance=null;
         if (instance == null) initialize();
         return instance;
     }
+    public static void removeInstance() { instance = null; }
+
+
 
     private static void initialize() {
+        instance = new GeografijaDAO();
     }
 
     public ArrayList<Grad> gradovi() {
@@ -20,9 +59,8 @@ public class GeografijaDAO {
         return grad;
     }
 
-    public Grad glavniGrad(String bosna_i_hercegovina) {
-        Grad glavni=null;
-        return glavni;
+    public Grad glavniGrad(String glavniGrad) {
+
     }
 
 
