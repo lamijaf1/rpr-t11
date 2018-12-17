b90a12a4e41fa5a1b80f4e4bd9437846d599b6e3
Lamija <lfazlija1@etf.unsa.ba>
2018-12-17 23:34:07 +0100

    dodane metode u mainu createNewDataBase i createNewTable

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,9 +1,6 @@
 package ba.unsa.etf.rpr;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,9 +20,37 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/baza.db";
+
+        // SQL statement for creating a new table
+        String grad = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id int PRIMARY KEY,\n"
+                + "	naziv text NOT NULL,\n"
+                + "	CONSTRAINT grad_drzava_glavni_grad_fk FOREIGN KEY (id) REFERENCES drzava (glavni_grad)\n"
+                + ");";
+
+        String drzava= "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id int PRIMARY KEY,\n"
+                + "	naziv text NOT NULL,\n"
+                + "	CONSTRAINT drzava_grad_broj_stanovnika_fk FOREIGN KEY (id) REFERENCES grad (broj_stanovnika)\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement();
+             Statement stmt1 = conn.createStatement()) {
+            // create a new table
+            stmt.execute(grad);
+            stmt1.execute(drzava);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         createNewDatabase("baza");
+        createNewTable();
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
     }
